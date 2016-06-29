58c3dc07e06c92024be2206257526a71c57f73f5
Craig DesBrisay <craig.desbrisay@gmail.com>
2016-06-29 02:27:54 +0000

    added navbar links and welcome controller

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,16 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def features
+  end
+
+  def pricing
+  end
+
+  def faq
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'welcome/about'
+  get 'welcome/features'
+  get 'welcome/pricing'
+  get 'welcome/faq'
+
   resources :invoices
+  
+  root to: 'welcome#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
